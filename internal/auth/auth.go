2a7e69c5bb334d21b6d6aa58a4cdb56feb81a177
Akhil TN <tnakhil@gmail.com>
2024-07-14 21:57:11 +0530

    api key implementation

---
+++ internal/auth/auth.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+// getAPIKey() extracts an API key from
+// the header of an HTTP request
+// Example:
+// Authorization: ApiKey {insert apikey here}
+func GetAPIKey(headers http.Header) (string, error) {
+	val := headers.Get("Authorization")
+	if val == "" {
+		return "", errors.New("no Authorization info found")
+	}
+
+	vals := strings.Split(val, " ")
+	if len(vals) != 2 {
+		return "", errors.New("malformed auth header")
+	}
+	if vals[0] != "ApiKey" {
+		return "", errors.New("malformed auth header")
+	}
+	return vals[1], nil
+}
