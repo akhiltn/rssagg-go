67328149796fb93e5949d5aede9cc55e02b548ae
Akhil TN <tnakhil@gmail.com>
2024-07-14 15:36:41 +0530

    goose files

---
+++ schema/001_users.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+
+create table users (
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  name TEXT NOT NULL
+);
+
+-- +goose down
+
+DROP TABLE users;
